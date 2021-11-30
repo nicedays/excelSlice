1951c95c49d1f08e7b8ab7595c133ad89add43fd
nicedays <ikevinsama@gmail.com>
2021-11-30 22:36:53 +0800

    第一次提交

---
+++ 农邦分表器/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace 农邦分表器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(222, 116);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "分表规则文件";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(210, 84);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请拖动文件到这里";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.DragDrop += new System.Windows.Forms.DragEventHandler(this.label1_DragDrop);
+            this.label1.DragEnter += new System.Windows.Forms.DragEventHandler(this.label1_DragEnter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(283, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(222, 116);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "总表文件";
+            // 
+            // label2
+            // 
+            this.label2.AllowDrop = true;
+            this.label2.Location = new System.Drawing.Point(6, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 84);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "请拖动文件到这里";
+            this.label2.DragDrop += new System.Windows.Forms.DragEventHandler(this.label2_DragDrop);
+            this.label2.DragEnter += new System.Windows.Forms.DragEventHandler(this.label2_DragEnter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "分表规则对应总表的列号";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(162, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(72, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(291, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "一键分表";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(526, 195);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "农邦分表器";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 农邦分表器/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using Microsoft.Office.Interop.Excel;
+using MSExcel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Core;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Reflection;
+
+
+
+
+
+
+/***
+ *  anthor:nicedays   ikevinsama@gmail.com  *
+ *    **/
+namespace 农邦分表器
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = GetValue("NUM");
+         }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        private void label1_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        private void label1_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            //其中label1.Text显示的就是拖进文件的文件名；
+
+            label1.Text = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+        }
+        private void label2_DragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Link;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        private void label2_DragDrop(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            //其中label1.Text显示的就是拖进文件的文件名；
+
+            label2.Text = ((System.Array)e.Data.GetData(DataFormats.FileDrop)).GetValue(0).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SetValue("NUM", textBox1.Text.ToString());
+            
+            CutExcel(label1.Text.ToString(), label2.Text.ToString());
+        }
+
+        /// <summary>
+        /// 分割excel
+        /// 
+        /// </summary>
+        /// <param name="rule">规则表</param>
+        /// <param name="date">总表</param>
+        /// <returns></returns>
+        public int CutExcel(string rule,string date)
+        {
+            FileInfo fi = new FileInfo(rule);
+            FileInfo fi2 = new FileInfo(date);
+            if (fi.Exists && fi2.Exists)
+            {
+                Console.WriteLine(label1.Text.ToString());
+                //创建
+                MSExcel.Application xlApp = new MSExcel.Application();
+                xlApp.DisplayAlerts = false;
+                xlApp.Visible = false;
+                xlApp.ScreenUpdating = false;
+                //打开Excel
+                MSExcel.Workbook xlsWorkBook = xlApp.Workbooks.Open(rule, System.Type.Missing, System.Type.Missing, System.Type.Missing,
+                System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing,
+                System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing);
+
+                //处理数据过程
+                MSExcel.Worksheet m_Sheet = xlsWorkBook.Worksheets[1];//工作薄从1开始，不是0
+                int Flag = m_Sheet.UsedRange.Rows.Count;
+                int Flag2 = m_Sheet.UsedRange.Columns.Count;
+                string[][] ruleSheet = new string[Flag2][];
+                //读取规则excel表得内容存在数组当中
+                for (int i = 0; i < Flag2; i++)
+                {
+                    ruleSheet[i] = new string[Flag];
+                    for (int j = 0; j < Flag - 1; j++)
+                    {
+                        ruleSheet[i][j] = m_Sheet.Cells[j + 2, i + 1].Text;
+
+                    }
+                }
+                button1.Text = "数据处理中，请等待";
+                string Pathstr = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase+"xlsx";
+                if (!Directory.Exists(Pathstr))
+                {
+
+                    DirectoryInfo directoryInfo = new DirectoryInfo(Pathstr);
+                    directoryInfo.Create();
+                }
+                
+                //分割表
+                for (int i = 0; i < m_Sheet.UsedRange.Columns.Count; i++)
+                {
+                    //打开总表开始处理数据
+                    MSExcel.Workbook xlsWorkBook2 = xlApp.Workbooks.Open(date, System.Type.Missing, System.Type.Missing, System.Type.Missing,
+                    System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing,
+                    System.Type.Missing, System.Type.Missing, System.Type.Missing, System.Type.Missing);
+                    MSExcel.Worksheet m_Sheet2 = xlsWorkBook2.Worksheets[1];//工作薄从1开始，不是0
+                    int row_flag = m_Sheet2.UsedRange.Rows.Count ;//记录行号
+
+                    for (int j = 2; j < row_flag+2; j++)
+                    {
+                        string PeisongAddress = m_Sheet2.Cells[j, int.Parse(textBox1.Text.ToString())].text;
+                        
+                        //判断配送点是否在地区当中,不在就删除行
+                        if (((IList)ruleSheet[i]).Contains(PeisongAddress)&&(PeisongAddress!=""))
+                        {
+
+                            //Range mergeArea = m_Sheet2.Range[m_Sheet2.Cells[17,6], m_Sheet2.Cells[17, 6]].MergeArea;
+                            //Console.WriteLine(mergeArea.Cells.Rows.Count);
+                            //new_Sheet.Range[new_Sheet.Cells[row_flag, 1]
+                            //    , new_Sheet.Cells[row_flag, m_Sheet2.UsedRange.Columns.Count]]
+                            //    .Copy(m_Sheet2.Range[m_Sheet2.Cells[j + 1, 1], m_Sheet2.Cells[j + 1, m_Sheet2.UsedRange.Columns.Count]]);
+                            //m_Sheet2.Range[m_Sheet2.Cells[j + 1, 1], m_Sheet2.Cells[j + 1, m_Sheet2.UsedRange.Columns.Count]].Copy();
+                            //new_Sheet.Range[row_flag, 1].PasteSpecial();
+                            //Range data_range = m_Sheet2.Range[m_Sheet2.Cells[j + 1, 1], m_Sheet2.Cells[j + 1, m_Sheet2.UsedRange.Columns.Count]];
+                            // Range new_range = new_Sheet.Range[new_Sheet.Cells[row_flag, 1]
+                            //    , new_Sheet.Cells[row_flag, m_Sheet2.UsedRange.Columns.Count]];
+                            // data_range.Copy(Type.Missing);
+                            // new_range.PasteSpecial(Microsoft.Office.Interop.Excel.XlPasteType.xlPasteFormulas, Microsoft.Office.Interop.Excel.XlPasteSpecialOperation.xlPasteSpecialOperationNone, false, false);
+                            // Console.WriteLine(m_Sheet2.Cells[j + 1, 1].text);
+                            // row_flag++;
+
+
+                        }
+                        else if((!((IList)ruleSheet[i]).Contains(PeisongAddress)) && (PeisongAddress != ""))
+                        {
+                            
+                            m_Sheet2.Rows[j].Delete();
+                            
+                            j--;
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                    string FilePath=Pathstr+"\\"+m_Sheet.Cells[1,i+1].Text+ DateTime.Now.ToString("yyyyMMddhhmm") + ".XLSX";
+                    FilePath = FilePath.Replace("\\\\", "\\");
+                    FileInfo fii = new FileInfo(FilePath);
+                    if (fii.Exists)     //判断文件是否已经存在,如果存在就删除!
+                    {
+                        fii.Delete();
+                    }
+                    xlsWorkBook2.SaveAs(fii, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, XlSaveAsAccessMode.xlNoChange,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+                    xlsWorkBook2.Close(true, Type.Missing, Type.Missing);
+                    // PrintSheet(m_Sheet2);
+                    // Console.WriteLine("====");
+
+                }
+
+
+
+                button1.Text = "处理完毕";
+                ClosePro(xlApp, xlsWorkBook);
+
+
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// 打印一个表的所有信息
+        /// </summary>
+        /// <param name="m_Sheet">sheet</param>
+        public void PrintSheet(MSExcel.Worksheet m_Sheet)
+        {
+            
+            
+                for (int i = 0; i < m_Sheet.UsedRange.Rows.Count; i++)
+                {
+                    for(int j = 0; j < m_Sheet.UsedRange.Columns.Count; j++)
+                    {
+                        Console.Write(m_Sheet.Cells[i+1,j+1].Text + "\t");
+                    }
+                    Console.WriteLine("");
+                }
+                
+        }
+
+        /// <summary>
+        /// 配置文件键值对
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        public static void SetValue(string key, string value)
+        {
+            //增加的内容写在appSettings段下 <add key="RegCode" value="0"/>  
+            System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (config.AppSettings.Settings[key] == null)
+            {
+                config.AppSettings.Settings.Add(key, value);
+            }
+            else
+            {
+                config.AppSettings.Settings[key].Value = value;
+            }
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");//重新加载新的配置文件   
+        }
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetWindowThreadProcessId(IntPtr hwnd, out int ID);
+        /// <summary>
+        /// 关闭进程
+        /// </summary>
+        /// <param name="xlApp"></param>
+        /// <param name="xlsWorkBook"></param>
+        public void ClosePro(MSExcel.Application xlApp, MSExcel.Workbook xlsWorkBook)
+        {
+            if (xlsWorkBook != null)
+                xlsWorkBook.Close(true, Type.Missing, Type.Missing);
+            xlApp.Quit();
+            // 安全回收进程
+            System.GC.GetGeneration(xlApp);
+            IntPtr t = new IntPtr(xlApp.Hwnd);   //获取句柄
+            int k = 0;
+            GetWindowThreadProcessId(t, out k);   //获取进程唯一标志
+            System.Diagnostics.Process p = System.Diagnostics.Process.GetProcessById(k);
+            p.Kill();     //关闭进程
+        }
+        /// <summary>  
+        /// 读取文件配置信息
+        /// </summary>  
+        /// <param name="key"></param>  
+        /// <returns></returns>  
+        public static string GetValue(string key)
+        {
+            System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            if (config.AppSettings.Settings[key] == null)
+                return "";
+            else
+                return config.AppSettings.Settings[key].Value;
+        }
+        
+    }
+}
